7b70caacbaa59e987a6cf987d8c807c185eecd3e
somebodies1 <56944237+somebodies1@users.noreply.github.com>
2022-11-06 15:02:48 +0800

    updated base entity

--- Assets/Scripts/BaseEntity.cs
+++ Assets/Scripts/BaseEntity.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 [System.Serializable]
 public class BaseEntity : MonoBehaviour
 {
+    public enum ENTITY_TYPE
+    {
+        PLAYER,
+        ENEMY,
+        ENTITY_TYPE_NUM
+    }
+
+    //Name
     public string entityName;
 
+    //Health
     public int MaxHP;
-    public int CurrentHp;
+    public int CurrentHP;
+
+    //Mana
+    public int MaxMP;
+    public int CurrentMP;
+
+    //Type of entity
+    public ENTITY_TYPE entityType;
+
+    public bool isDead = false;
+
+    public BaseEntity(string _name, int _hp, int _mp, ENTITY_TYPE _type)
+    {
+        entityName = _name;
+        MaxHP = CurrentHP = _hp;
+        MaxMP = CurrentMP = _mp;
+        entityType = _type;
+    }
 }
