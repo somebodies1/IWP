0583059d5493a5a51c2859f408eedf24e0bda6ba
somebodies1 <56944237+somebodies1@users.noreply.github.com>
2022-11-06 14:36:48 +0800

    Main Files

---
+++ Assets/Scripts/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseEntity : MonoBehaviour
+{
+    public string entityName;
+
+    public int MaxHP;
+    public int CurrentHp;
+}
